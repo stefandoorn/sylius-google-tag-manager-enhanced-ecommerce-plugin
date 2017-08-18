ff3996084a967201194dab16fbb8fa39a003c9ae
Stefan Doorn <stefan@efectos.nl>
2017-08-18 17:26:43 +0200

    Improve code layout

--- src/Resolver/CheckoutStepResolver.php
+++ src/Resolver/CheckoutStepResolver.php
@@ -3,7 +3,6 @@
 namespace GtmEnhancedEcommercePlugin\Resolver;
 
 use GtmEnhancedEcommercePlugin\TagManager\CheckoutStep;
-use Sylius\Bundle\CoreBundle\Controller\OrderController;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -19,23 +18,37 @@ class CheckoutStepResolver
      */
     public function resolve(string $method, Request $request): ?int
     {
-        if ($method === 'summaryAction') {
-            return CheckoutStep::STEP_CART;
-        } elseif ($method === 'updateAction') {
-            $route = $request->get('_route');
-            if ($route === 'sylius_shop_checkout_address') {
-                return CheckoutStep::STEP_ADDRESS;
-            } elseif ($route === 'sylius_shop_checkout_select_shipping') {
-                return CheckoutStep::STEP_SHIPPING;
-            } elseif ($route === 'sylius_shop_checkout_select_payment') {
-                return CheckoutStep::STEP_PAYMENT;
-            } elseif ($route === 'sylius_shop_checkout_complete') {
-                return CheckoutStep::STEP_CONFIRM;
-            }
-        } elseif ($method === 'thankYouAction') {
-            return CheckoutStep::STEP_THANKYOU;
+        switch ($method) {
+            case 'summaryAction':
+                return CheckoutStep::STEP_CART;
+            case 'updateAction':
+                return $this->updateAction($request);
+            case 'thankYouAction':
+                return CheckoutStep::STEP_THANKYOU;
         }
 
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     * @return int|null
+     */
+    private static function updateAction(Request $request): ?int
+    {
+        $route = $request->get('_route');
+
+        switch ($route) {
+            case 'sylius_shop_checkout_address':
+                return CheckoutStep::STEP_ADDRESS;
+            case 'sylius_shop_checkout_select_shipping':
+                return CheckoutStep::STEP_SHIPPING;
+            case 'sylius_shop_checkout_select_payment':
+                return CheckoutStep::STEP_PAYMENT;
+            case 'sylius_shop_checkout_complete':
+                return CheckoutStep::STEP_CONFIRM;
+        }
+        
         return null;
     }
 }
