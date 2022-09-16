383511ae1dc6dcdd41db81914b0b4a22947be148
Stefan Doorn <stefan@efectos.nl>
2022-09-16 10:33:37 +0200

    Revert "Remove Sylius 1.8 configs"
    This reverts commit 6cd38eea641aec4cc5bb620cd12eddcf0038bd53.

---
+++ tests/Application/config/sylius/1.8/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
+];
