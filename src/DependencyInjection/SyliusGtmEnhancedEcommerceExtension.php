7dd9cf0dfab5c23073d45998e9615986da1d1453
Stefan Doorn <stefan@efectos.nl>
2023-06-28 11:08:24 +0200

    Remove PrependExtensionInterface usage

--- src/DependencyInjection/SyliusGtmEnhancedEcommerceExtension.php
+++ src/DependencyInjection/SyliusGtmEnhancedEcommerceExtension.php
@@ -7,10 +7,9 @@ namespace StefanDoorn\SyliusGtmEnhancedEcommercePlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class SyliusGtmEnhancedEcommerceExtension extends Extension implements PrependExtensionInterface
+final class SyliusGtmEnhancedEcommerceExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
