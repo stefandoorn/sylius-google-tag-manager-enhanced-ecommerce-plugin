12d45115c45dabf6e61bce62c2bdb15befd2df7c
Stefan Doorn <stefan@efectos.nl>
2024-01-03 10:05:30 +0100

    Conflict with Symfony <5.3, so we can safely use MainRequest

--- src/Helper/MainRequest/ControllerEventMainRequest.php
+++ src/Helper/MainRequest/ControllerEventMainRequest.php
@@ -10,14 +10,6 @@ final class ControllerEventMainRequest
 {
     public static function isMainRequest(ControllerEvent $event): bool
     {
-        if (\method_exists($event, 'isMainRequest')) {
-            return $event->isMainRequest();
-        }
-
-        if (\method_exists($event, 'isMasterRequest')) {
-            return $event->isMasterRequest();
-        }
-
-        throw new \Exception('Neither "isMainRequest" or "isMasterRequest" exists');
+        return $event->isMainRequest();
     }
 }
