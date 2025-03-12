be078818e3fbe488289b5ab53396a139ee37f00c
Francis Hilaire <Prometee@users.noreply.github.com>
2025-03-12 13:19:51 +0100

    Fix ECS

--- src/Helper/ProductIdentifierHelperInterface.php
+++ src/Helper/ProductIdentifierHelperInterface.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace StefanDoorn\SyliusGtmEnhancedEcommercePlugin\Helper;
 
 use Sylius\Component\Core\Model\ProductInterface;
+
 interface ProductIdentifierHelperInterface
 {
     public function getProductIdentifier(ProductInterface $product): string;
-
 }
