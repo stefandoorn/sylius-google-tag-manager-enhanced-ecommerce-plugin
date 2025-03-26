d3a7402b01d7dacb18c7f2b296868b4a58d6b2eb
Francis Hilaire <Prometee@users.noreply.github.com>
2025-03-26 11:50:41 +0100

    Change to PHP Session storage

--- src/DependencyInjection/SyliusGtmEnhancedEcommerceExtension.php
+++ src/DependencyInjection/SyliusGtmEnhancedEcommerceExtension.php
@@ -7,10 +7,9 @@ namespace StefanDoorn\SyliusGtmEnhancedEcommercePlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class SyliusGtmEnhancedEcommerceExtension extends Extension implements PrependExtensionInterface
+final class SyliusGtmEnhancedEcommerceExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -30,23 +29,5 @@ final class SyliusGtmEnhancedEcommerceExtension extends Extension implements Pre
                 $loader->load(\sprintf('features/%s.yaml', $feature));
             }
         }
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        $this->prependSymfonyConfig($container);
-    }
-
-    private function prependSymfonyConfig(ContainerBuilder $container): void
-    {
-        $container->prependExtensionConfig('framework', [
-            'cache' => [
-                'pools' => [
-                    'cache.sylius_gtm_enhanced_ecommerce' => [
-                        'adapter' => 'cache.app',
-                    ],
-                ],
-            ],
-        ]);
     }
 }
--- src/Service/CachedGoogleTagManager.php
+++ src/Service/CachedGoogleTagManager.php
@@ -4,57 +4,94 @@ declare(strict_types=1);
 
 namespace StefanDoorn\SyliusGtmEnhancedEcommercePlugin\Service;
 
-use Symfony\Component\Cache\Adapter\AdapterInterface;
-use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Contracts\Service\ResetInterface;
 use Xynnn\GoogleTagManagerBundle\Service\GoogleTagManagerInterface;
 
 final class CachedGoogleTagManager implements GoogleTagManagerInterface, ResetInterface
 {
-    public const GTM_DATA_LAYER = 'gtm_data_layer';
-
-    private bool $pushCalled = false;
+    public const GTM_CACHED_PUSH = 'gtm_cached_push';
 
     public function __construct(
         private GoogleTagManagerInterface $googleTagManager,
-        protected AdapterInterface&CacheInterface $cache,
+        protected RequestStack $requestStack,
     ) {
-        $cacheItem = $this->cache->getItem(self::GTM_DATA_LAYER);
-        /** @var array<string, array<string, mixed>> $cachedPushes */
-        $cachedPushes = $cacheItem->isHit() ? $cacheItem->get() : [];
-        foreach ($cachedPushes as $push) {
-            $this->addPush($push);
-        }
     }
 
     public function addPush($value): void
     {
-        $this->googleTagManager->addPush($value);
-
-        $cacheItem = $this->cache->getItem(self::GTM_DATA_LAYER);
-        $cacheItem->set($this->getPush());
-        $this->cache->save($cacheItem);
-
-        $this->pushCalled = false;
+        if (false === $this->cachePush($value)) {
+            $this->googleTagManager->addPush($value);
+        }
     }
 
     public function getPush(): array
     {
-        $this->pushCalled = true;
+        $this->addCachedPush();
+        $this->removeCachedPushed();
 
         return $this->googleTagManager->getPush();
+    }
+
+    private function cachePush(mixed $value): bool
+    {
+        $session = $this->getRequestSession();
+        if (null === $session) {
+            return false;
+        }
+
+        /** @var mixed[] $push */
+        $push = $session->get(self::GTM_CACHED_PUSH, []);
+        $push[] = $value;
+
+        $session->set(self::GTM_CACHED_PUSH, $push);
+
+        return true;
+    }
+
+    private function addCachedPush(): void
+    {
+        $session = $this->getRequestSession();
+        if (null === $session) {
+            return;
+        }
+
+        /** @var array<string, array<string, mixed>> $cachedPushes */
+        $cachedPushes = $session->get(self::GTM_CACHED_PUSH, []);
+
+        foreach ($cachedPushes as $push) {
+            $this->googleTagManager->addPush($push);
+        }
+    }
+
+    private function removeCachedPushed(): void
+    {
+        $session = $this->getRequestSession();
+        if (null === $session) {
+            return;
+        }
+
+        $session->remove(self::GTM_CACHED_PUSH);
+    }
+
+    private function getRequestSession(): ?SessionInterface
+    {
+        $mainRequest = $this->requestStack->getMainRequest();
+        if (null === $mainRequest) {
+            return null;
+        }
+
+        if (!$mainRequest->hasSession()) {
+            return null;
+        }
+
+        return $mainRequest->getSession();
     }
 
     public function reset(): void
     {
         $this->googleTagManager->reset();
-        if (false === $this->pushCalled) {
-            return;
-        }
-
-        $this->pushCalled = false;
-
-        $this->cache->delete(self::GTM_DATA_LAYER);
     }
 
     public function addData(string $key, mixed $value): void
