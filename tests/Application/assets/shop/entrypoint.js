4fe28c3429c09ab6b0186e68754d7e4115df16aa
Francis Hilaire <Prometee@users.noreply.github.com>
2026-04-13 11:12:07 +0200

    Migrate test application to sylius/test-application (#223)
    * test: add TestApplication configuration for GTM plugin testing
    * build: add asset entrypoints and controller configuration for GTM plugin
    * test: configure messenger transport DSNs for TestApplication
    * build: upgrade PHPStan to v2.x and clean up tracked files
    * ci: use SyliusLabs/BuildTestAppAction for CI workflow
    * build: remove custom bin-dir and use vendor/bin paths in composer script
    * fix: remove enable_native_lazy_objects for Doctrine ORM 2.x compatibility
    * chore: align TestApplication config with Sylius CmsPlugin conventions
    * chore: remove Flex-generated assets/controllers.json
    * chore: remove unused bin/ dir and root controllers.json
    * fix: add Symfony Flex config to prevent skeleton file generation
    * fix: disable Symfony Flex and exclude var/ from ECS

--- tests/Application/assets/shop/entrypoint.js
+++
@@ -1,2 +0,0 @@
-// In this file you can import assets like images or stylesheets
-console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
---
+++ tests/TestApplication/config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    Xynnn\GoogleTagManagerBundle\GoogleTagManagerBundle::class => ['all' => true],
+    GtmPlugin\GtmPlugin::class => ['all' => true],
+    StefanDoorn\SyliusGtmEnhancedEcommercePlugin\SyliusGtmEnhancedEcommercePlugin::class => ['all' => true],
+];
